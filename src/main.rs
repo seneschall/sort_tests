57f79753bca9cf14bdbad9ed7da6bf793c601404
Simon Sack <simon@p200300cebf14a53bfe0bee1cf5ef7d31.dip0.t-ipconnect.de>
2025-06-04 21:09:53 +0200

    finished merge sort with iterator
    Unfortunately it's still a bit inefficient because I match the iterators
    every time.

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,75 @@
+use core::panic;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use quickcheck_macros::quickcheck;
+
+    #[quickcheck]
+    fn sort_ints(xs: Vec<i32>) -> bool {
+        let mut ys = xs.clone();
+        let mut xs = xs.clone();
+        ys.sort_unstable();
+        merge_sort(&mut xs);
+        xs == ys
+    }
+}
+
 pub fn merge_sort<T>(list: &mut [T])
 where
     T: Ord + Clone + Default + std::fmt::Debug,
 {
-    let length = list.len();
-    let mut result_buf = vec![T::default(); length]; // buffer the result will be written to
-                                                     // sort(list, 0, length / 2, length);
-
-    sort(list, 0, length, &mut result_buf);
-
-    for i in 0..length {
-        list[i] = result_buf[i].clone();
-    }
+    sort(list);
 }
 
-fn sort<T>(list: &[T], lo: usize, hi: usize, result_buf: &mut [T])
+fn sort<T>(list: &mut [T])
 where
     T: Ord + Clone + Default + std::fmt::Debug,
 {
-    if hi - lo <= 1 {
+    if list.len() <= 1 {
         return;
     }
 
-    let mid: usize = (lo + hi) / 2;
+    let mid: usize = list.len() / 2;
 
-    sort(list, lo, mid, result_buf);
-    sort(list, mid, hi, result_buf);
-    merge(list, lo, mid, hi, result_buf);
-    println!("{result_buf:?}");
+    sort(&mut list[..mid]);
+    sort(&mut list[mid..]);
+    merge(&mut list[..]);
 }
 
-fn merge<T>(list: &[T], lo: usize, mid: usize, hi: usize, result_buf: &mut [T])
+fn merge<T>(list: &mut [T])
 where
     T: Ord + Clone + Default + std::fmt::Debug,
 {
-    let mut buf_idx = lo;
+    let mid: usize = list.len() / 2;
+    let mut left = list[0..mid].to_vec().into_iter();
+    let mut right = list[mid..].to_vec().into_iter();
 
-    let mut left = list[lo..=mid].iter();
-    let mut right = list[mid + 1..hi].iter();
+    let mut a = left.next();
+    let mut b = right.next();
 
-    let mut l = left.next();
-    let mut r = left.next();
-
-    loop {
-        match (l, r) {
-            (None, None) => break,
-            (Some(a), None) => {
-                result_buf[buf_idx] = a.clone();
-                buf_idx += 1;
-                break;
+    for i in 0..list.len() {
+        match (&mut a, &mut b) {
+            (None, None) => panic!("Shouldn't happen."),
+            (Some(l), None) => {
+                list[i] = l.clone();
+                a = left.next();
             }
-            (None, Some(b)) => {
-                result_buf[buf_idx] = b.clone();
-                buf_idx += 1;
-                break;
+            (None, Some(r)) => {
+                list[i] = r.clone();
+                b = right.next();
             }
-            (Some(a), Some(b)) => {
-                if a <= b {
-                    result_buf[buf_idx] = a.clone();
-                    buf_idx += 1;
-                    l = left.next();
+            (Some(l), Some(r)) => {
+                if l <= r {
+                    list[i] = l.clone();
+                    a = left.next();
                 } else {
-                    result_buf[buf_idx] = b.clone();
-                    buf_idx += 1;
-                    r = right.next();
+                    list[i] = r.clone();
+                    b = right.next();
                 }
             }
         }
     }
-
-    for elem in left {
-        result_buf[buf_idx] = elem.clone();
-        buf_idx += 1;
-    }
-
-    for elem in right {
-        result_buf[buf_idx] = elem.clone();
-        buf_idx += 1;
-    }
-
-    // while lft_idx <= mid && rgt_idx < hi {
-    //     if list[lft_idx] <= list[rgt_idx] {
-    //         result_buf[buf_idx] = list[lft_idx].clone();
-    //         lft_idx += 1;
-    //     } else {
-    //         result_buf[buf_idx] = list[rgt_idx].clone();
-    //         rgt_idx += 1;
-    //     }
-    // }
 }
 
 fn main() {
